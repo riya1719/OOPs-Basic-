7896268370565042dc95f66f44a34907166ee3b3
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-04-22 18:46:22 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -12,24 +12,24 @@ public class Factorial {
 
 static int factorial ( int n)
 {
-		int Fact=1;
+int Fact=1;
 
 if (n == 0 || n ==1)
 return 1;
 
-		else
-		{			
-			for (int i = n; i > 1; i--)
-			 Fact = Fact * i;
-
-			return Fact;
-		}
+else
+{			
+ for (int i = n; i > 1; i--)
+ Fact = Fact * i;
+ return Fact;
+}
 
 
 }
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		
-		Scanner scan =new Scanner (System.in);
+	Scanner scan =new Scanner (System.in);
 	            
          // Take input as number from user
 		System.out.print("Enter the number: ");
