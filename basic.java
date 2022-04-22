7010721a8b1aa2dca678d790f2a95d21de180bfc
riya1719 <101990431+riya1719@users.noreply.github.com>
2022-04-22 20:02:10 +0530

    Add files via upload

---
+++ basic.java
@@ -0,0 +1,31 @@
+package demo;
+
+public class basic {
+	
+	public static void main(String[] args) throws Exception
+	{
+		// float num = 5.5;
+		//float num = 5.5 f;
+		 
+		float num1 = 5.5f;
+		float num2 = 5.5F;
+		
+		System.out.println(num1);
+		System.out.println(num2);
+		
+		//long num3 = 5.5;
+		
+		long num3 = 1234567891;
+		long num4 = 1234567891234567891l;
+		long num5 = 1234567891234567891L;
+		
+		
+		System.out.println(num3);
+		System.out.println(num4);
+		System.out.println(num5);
+
+		
+		
+		
+	}
+}
